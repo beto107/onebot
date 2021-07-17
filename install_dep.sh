1f34af8075aee2bb3a884f552f9939d43057c572
Andley S <39422775+Andley302@users.noreply.github.com>
2021-07-17 11:31:15 -0300

    Update install_dep.sh

--- install_dep.sh
+++ install_dep.sh
@@ -4,7 +4,8 @@ sudo apt-get install software-properties-common -y;
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -;
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list';
 cd /root;
-wget -q https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash install.sh;
+wget -q https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh
+bash install.sh;
 source ~/.bashrc;
 nvm install 16.4.2;
 sudo apt update;
